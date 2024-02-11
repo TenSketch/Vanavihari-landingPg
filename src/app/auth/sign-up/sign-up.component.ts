51c19d975ad79acb63e4a7570da11d667a04ba24
vig-nesh-waran <vigneshwarans1102@gmail.com>
2024-02-10 22:43:58 -0800

    Test Code Verification

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 
 @Component({
@@ -8,14 +11,38 @@ import { Router } from '@angular/router';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-  constructor(private router: Router) { }
+  code: any;
+  constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
-    // this.openModal();
+      this.route.queryParams.subscribe(params => {
+        this.code = params['code'];
+        // if(this.code !== 'undefined') {
+        //   window.location.href="https://accounts.zoho.com/oauth/v2/auth?response_type=code&client_id=1000.GW70XWAC3O04CJ67TUTEAYEOVP7RIM&client_secret=532929ef83d5a2b57ceb5f5ddb3f94e0ebb30b7ebc&scope=ZohoCreator.form.CREATE&redirect_uri=https://tensketch.vanavihari.com/register.html&access_type=offline";
+        // }
+      });
   }
-  goToSignup() {
-    this.router.navigate(['/sign-up']);
+
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      console.log(form.value);
+      console.log(this.code);
+
+  
+      this.http.post<any>('/api/oauth/v2/token?grant_type=authorization_code&client_id=1000.GW70XWAC3O04CJ67TUTEAYEOVP7RIM&client_secret=532929ef83d5a2b57ceb5f5ddb3f94e0ebb30b7ebc&redirect_uri=https://tensketch.vanavihari.com/register.html&code=' + this.code, {})
+      .subscribe(
+        response => {
+          console.log("Response:", response);
+        },
+        error => {
+          console.error("Error:", error);
+        }
+      );
+    
+
+    }
   }
+  
   goToSignin() {
     this.router.navigate(['/sign-in']);
   }
