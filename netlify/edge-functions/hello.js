9dbaa4af103db9a92a09b8a8c0f58bb6f5ecc988
Venkat408 <venkat408prabhu@gmail.com>
2024-03-01 18:04:04 +0530

    test 9

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,70 +1,38 @@
-import fetch from 'node-fetch';
-
 export default async (req) => {
-    try {
-        const { query } = req;
-        if (!query.api_type) {
-            return new Response(JSON.stringify({ error: 'Missing required parameters' }), {
-                status: 400,
-                headers: { 'Content-Type': 'application/json' },
-            });
-        }
-
-        const apiType = query.api_type;
-        const fullname = query.fullname;
-        const username = query.username;
-        const email = query.email;
-        const mobile = query.mobile;
-        const password = query.password;
-        const token = query.token;
-
-        let apiUrl = '';
-        let method = '';
-
-        switch (apiType) {
-            case 'registration':
-                apiUrl = `https://www.zohoapis.com/creator/custom/vanavihari/Account_Registration?publickey=8xZYn5bvUfjjBVVpvK7qAsKsR&full_name=${fullname}&email=${email}&mobile=${mobile}&password=${password}`;
-                method = 'GET';
-                break;
-            case 'login':
-                apiUrl = `https://www.zohoapis.com/creator/custom/vanavihari/Login_Validation?publickey=${publickey}&user_name=${username}&password=${password}`;
-                method = 'GET';
-                break;
-            case 'email_verification':
-                apiUrl = `https://www.zohoapis.com/creator/custom/vanavihari/Email_Verification?publickey=fArmqypVSku88tfArkejTR5wq&token=${token}`;
-                method = 'GET';
-                break;
-            default:
-                return new Response(JSON.stringify({ error: 'Invalid API parameter' }), {
-                    status: 400,
-                    headers: { 'Content-Type': 'application/json' },
-                });
-        }
-
-        const response = await fetch(apiUrl, {
-            method: method,
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*', // Allow requests from any origin
-            },
-            // If sending data in the request body for POST requests
-            // body: JSON.stringify({ key: 'value' }),
-        });
-
-        const data = await response.json();
-
-        return new Response(JSON.stringify(data), {
-            headers: { 'Content-Type': 'application/json' },
-        });
-    } catch (error) {
-        console.error('Error:', error);
-        return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
-            status: 500,
+    const { query } = req;
+    if (!query.api_type) {
+        return new Response(JSON.stringify({ error: 'Missing required parameters' }), {
+            status: 400,
             headers: { 'Content-Type': 'application/json' },
         });
     }
-};
+
+    const apiType = query.api_type;
+    const fullname = query.fullname;
+    const username = query.username;
+    const email = query.email;
+    const mobile = query.mobile;
+    const password = query.password;
+    const token = query.token;
+
+    let apiUrl = '';
+    let method = '';
+
+    
+    const response = await fetch('https://www.zohoapis.com/creator/custom/vanavihari/Login_Validation?publickey=3gJbpvFUR8pR3knE8u0tMtt8p&user_name=venkat408prabhu@gmail.com&password=123456', {
+        method: 'GET', // or 'POST'
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            // 'Access-Control-Allow-Origin': 'http://example.com',  // Use this line to allow specific domain
+        },
+    });
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
 
 export const config = {
     path: "/zoho-connect"
-};
+}
