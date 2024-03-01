6e5198f7516890236930a83f9c3e700abf5262a9
Venkat408 <venkat408prabhu@gmail.com>
2024-03-01 17:09:02 +0530

    test

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,9 +1,25 @@
-// export default () => new Response("Hello world");
-export default async () => {
-    console.log('test log');
-    return new Response("Hello, World 2!", {
-      headers: { "content-type": "text/html" }
-    });
-  };
-  
-export const config = { path: "/test" };
+// apiCallFunction.js
+
+const axios = require('axios');
+
+module.exports = async (event, context) => {
+  try {
+    // Make an API call
+    const response = await axios.get('https://www.zohoapis.com/creator/custom/vanavihari/Login_Validation?publickey=3gJbpvFUR8pR3knE8u0tMtt8p&user_name=venkat408prabhu@gmail.com&password=123456');
+
+    // Extract data from the API response
+    const responseData = response.data;
+
+    // Return a successful response
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data: responseData }),
+    };
+  } catch (error) {
+    // Return an error response if the API call fails
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
