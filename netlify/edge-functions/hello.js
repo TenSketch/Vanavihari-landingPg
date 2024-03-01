cb31f022f289c4307c6413511af6ea33d3e9eb98
Venkat408 <venkat408prabhu@gmail.com>
2024-03-01 16:08:09 +0530

    test netlify api

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,3 +1,8 @@
-export default () => new Response("Hello world");
-
+// export default () => new Response("Hello world");
+export default async () => {
+    return new Response("Hello, World!", {
+      headers: { "content-type": "text/html" }
+    });
+  };
+  
 export const config = { path: "/test" };
