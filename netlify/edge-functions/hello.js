560cdfb8aa2f1c4d861c5589cb1d41b163ebf382
Venkat408 <venkat408prabhu@gmail.com>
2024-03-01 18:05:27 +0530

    test 10

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -18,19 +18,26 @@ export default async (req) => {
     let apiUrl = '';
     let method = '';
 
-    
-    const response = await fetch('https://www.zohoapis.com/creator/custom/vanavihari/Login_Validation?publickey=3gJbpvFUR8pR3knE8u0tMtt8p&user_name=venkat408prabhu@gmail.com&password=123456', {
-        method: 'GET', // or 'POST'
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            // 'Access-Control-Allow-Origin': 'http://example.com',  // Use this line to allow specific domain
-        },
-    });
-    const data = await response.json();
-    return new Response(JSON.stringify(data), {
-        headers: { 'Content-Type': 'application/json' },
-    });
+    try {
+        const response = await fetch('https://www.zohoapis.com/creator/custom/vanavihari/Login_Validation?publickey=3gJbpvFUR8pR3knE8u0tMtt8p&user_name=venkat408prabhu@gmail.com&password=123456', {
+            method: 'GET', // or 'POST'
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                // 'Access-Control-Allow-Origin': 'http://example.com',  // Use this line to allow specific domain
+            },
+        });
+        const data = await response.json();
+        return new Response(JSON.stringify(data), {
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
 }
 
 export const config = {
