43cd83bd7fe1bee3c13d233ed2dc75d200c9d975
Venkat408 <venkat408prabhu@gmail.com>
2024-03-01 18:00:57 +0530

    test 8

--- netlify/edge-functions/zohoConnect.js
+++ netlify/edge-functions/zohoConnect.js
@@ -1,18 +1,3 @@
-export default async (req) => {
-    const response = await fetch('https://www.zohoapis.com/creator/custom/vanavihari/Login_Validation?publickey=3gJbpvFUR8pR3knE8u0tMtt8p&user_name=venkat408prabhu@gmail.com&password=123456', {
-        method: 'GET', // or 'POST'
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            // 'Access-Control-Allow-Origin': 'http://example.com',  // Use this line to allow specific domain
-        },
-    });
-    const data = await response.json();
-    return new Response(JSON.stringify(data), {
-        headers: { 'Content-Type': 'application/json' },
-    });
-}
+export default () => new Response("Hello world");
 
-export const config = {
-    path: "/zoho-connect"
-}
+export const config = { path: "/test" };
