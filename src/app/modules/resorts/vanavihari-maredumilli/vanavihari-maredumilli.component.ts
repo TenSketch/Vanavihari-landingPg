88f3b3b0e683b63f1480ecd904fbd58660300482
Kaviyameena <kaviyameena0411@gmail.com>
2024-03-06 19:17:52 +0530

    listing page dynamic content

--- src/app/modules/resorts/vanavihari-maredumilli/vanavihari-maredumilli.component.ts
+++ src/app/modules/resorts/vanavihari-maredumilli/vanavihari-maredumilli.component.ts
@@ -10,8 +10,17 @@ import { Router } from '@angular/router';
 export class VanavihariMaredumilliComponent {
   selectedSortOption: string;
   showBookingSummary: boolean = false;
+  roomCards: any[] = Array.from({ length: 4 }, (_, index) => ({
+    roomName: 'Wood Pecker',
+    cottageType: 'Wooden Cottages',
+    bedType: '1 King Size double bed',
+    amenities: ['AC', 'Geyser', 'Western Toilet'],
+    rating: 'Good',
+    price: '1 night, 3 adults, 1 child 4500 INR + GST @ 18%  Extra bed @ 1500 INR',
+    image: 'assets/img/jungle.jpeg',
+  }));
   constructor(private router: Router) {
-    // Initialize default value
+  
     this.selectedSortOption = 'lowToHigh';
   }
 
@@ -20,5 +29,8 @@ export class VanavihariMaredumilliComponent {
   }
   goToBooking() {
     this.router.navigate(['/booking-summary']);
+  }
+  trackByRoomCard(index: number, card: any): string {
+    return card.roomName;
   }
 }
