bbdb48d31f0a8575c18da6b05d29993e82227d3a
Venkat408 <venkat408prabhu@gmail.com>
2024-03-19 19:00:25 +0530

    myprofile/Booking Summary - changes 6 - 19-03-2023

--- src/app/modules/booking-summary/booking-summary.component.ts
+++ src/app/modules/booking-summary/booking-summary.component.ts
@@ -15,14 +15,41 @@ export class BookingSummaryComponent {
   form: FormGroup;
   adultsCount: number = 1;
   childrenCount: number = 0;
+  guestCount: number = 0;
   roomsCount: number = 1;
+  roomDetails: any[];
   checkInDate: string;
   checkOutDate: string;
-  durationOfStay: number = 1;
+  durationOfStay: string = '';
   seslectedResort: string;
   getFullUser: string;
+  maxAdultCount: number = 2;
+  maxChildrenCount: number = 1;
+  totalPrice: number = 0;
+  totalGSTPrice: number = 0;
+  roomGuestDetails: any[] = [];
   constructor(private router: Router, private authService: AuthService, private http: HttpClient, private formBuilder: FormBuilder, private userService: UserService, private snackBar: MatSnackBar) {
-    console.log(this.authService.getBookingRooms());    
+    this.roomDetails = this.authService.getBookingRooms();
+    if(this.roomDetails.length > 0) {
+      console.log(this.roomDetails);
+      this.adultsCount = 0;
+      this.childrenCount = 0;
+      this.guestCount = 0;
+      this.totalPrice = 0;
+      this.totalGSTPrice = 0;
+      for (const room of this.roomDetails) {
+        if(parseInt(room.noof_guest)>0) {
+          this.roomGuestDetails.push(room.id, room.noof_guest);
+        }
+        this.adultsCount += parseInt(room.noof_adult);
+        this.childrenCount += parseInt(room.noof_child);
+        this.guestCount += parseInt(room.noof_guest);
+
+        this.totalPrice += parseInt(room.week_day_rate+(room.noof_guest*room.week_day_bed_charge));
+        this.totalGSTPrice += (parseInt(room.week_day_rate+(room.noof_guest*room.week_day_bed_charge))*12)/100;
+      }
+    }
+    
     this.form = this.formBuilder.group({
       gname: [''],
       gphone: [''],
@@ -38,43 +65,61 @@ export class BookingSummaryComponent {
     this.checkInDate = this.authService.getSearchData('checkin');
     this.checkOutDate = this.authService.getSearchData('checkout');
     this.seslectedResort = this.authService.getSearchData('resort');
+
+    const startDate = this.parseDate(this.checkInDate);
+    const endDate = this.parseDate(this.checkOutDate);
+    const durationMs = endDate.getTime() - startDate.getTime();
+    const days = Math.floor(durationMs / (1000 * 60 * 60 * 24));
+    // const weeks = Math.floor(durationMs / (1000 * 60 * 60 * 24 * 7));
+    this.durationOfStay = `${days} day${days>1?'s':''}`;
+
     this.getFullUser = this.userService.getFullUser();
+    
     const params = new HttpParams()
       .set('email', this.authService.getAccountUsername()??'')
       .set('token', this.authService.getAccessToken()??'');
-    this.http.get<any>('https://vanavihari-ng.netlify.app/zoho-connect?api_type=profile_details', {params}).subscribe({
-      next: response => {
-        if(response.code == 3000 && response.result.status == 'success') {
-          this.form = this.formBuilder.group({
-            gname: [response.result.name],
-            gphone: [response.result.phone],
-            gemail: [response.result.email, Validators.email],
-            dob: [response.result.dob, Validators.required],
-            nationality: [response.result.nationality],
-            gaddress: [response.result.address1],
-            address2: [response.result.address2],
-            gcity: [response.result.city],
-            gstate: [response.result.state],
-            gpincode: [response.result.pincode],
-            gcountry: [response.result.country]
-          });
-        } else if (response.code == 3000) {
-          this.userService.clearUser();
-          alert('Login Error!');
-          // this.router.navigate(['/home']);
-        } else {
-          this.userService.clearUser();
-          alert('Login Error!');
-          // this.router.navigate(['/home']);
-        }
-      },
-      error: err => {
-        console.error('Error:', err);
-      }
-    });
+    // this.http.get<any>('https://vanavihari-ng.netlify.app/zoho-connect?api_type=profile_details', {params}).subscribe({
+    //   next: response => {
+    //     if(response.code == 3000 && response.result.status == 'success') {
+    //       this.form = this.formBuilder.group({
+    //         gname: [response.result.name],
+    //         gphone: [response.result.phone],
+    //         gemail: [response.result.email, Validators.email],
+    //         dob: [response.result.dob, Validators.required],
+    //         nationality: [response.result.nationality],
+    //         gaddress: [response.result.address1],
+    //         address2: [response.result.address2],
+    //         gcity: [response.result.city],
+    //         gstate: [response.result.state],
+    //         gpincode: [response.result.pincode],
+    //         gcountry: [response.result.country]
+    //       });
+    //     } else if (response.code == 3000) {
+    //       this.userService.clearUser();
+    //       alert('Login Error!');
+    //       // this.router.navigate(['/home']);
+    //     } else {
+    //       this.userService.clearUser();
+    //       alert('Login Error!');
+    //       // this.router.navigate(['/home']);
+    //     }
+    //   },
+    //   error: err => {
+    //     console.error('Error:', err);
+    //   }
+    // });
 
 
   }
+  parseDate(dateString: string): Date {
+    const parts = dateString.split('-');
+    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    const monthIndex = months.findIndex(m => m === parts[1]);
+    const day = parseInt(parts[0], 10);
+    const year = parseInt(parts[2], 10);
+    return new Date(year, monthIndex, day);
+  }
+  
   isLoggedIn(): boolean {
     return this.userService.isLoggedIn();
   }
@@ -94,9 +139,10 @@ export class BookingSummaryComponent {
       .set('checkout', this.checkOutDate)
       .set('resort', this.seslectedResort)
       .set('selected_rooms', room_ids)
-      .set('noof_adult', 1)
-      .set('noof_child', 0)
-      .set('noof_guest', 0);
+      .set('room_guest_details', this.roomDetails.map(item => `${item.id}-${item.noof_guest}`).join(','))
+      .set('noof_adult', this.adultsCount)
+      .set('noof_child', this.childrenCount)
+      .set('noof_guest', this.guestCount);
       Object.keys(this.form.value).forEach((key) => {
         params = params.append(key, this.form.value[key]);
       });
