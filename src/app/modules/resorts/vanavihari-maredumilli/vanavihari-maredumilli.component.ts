9fb4b7af78322ba490d4372dd24d09cb7332f314
Venkat408 <venkat408prabhu@gmail.com>
2024-03-19 19:04:00 +0530

    myprofile/Booking Summary - changes 7 - 19-03-2023

--- src/app/modules/resorts/vanavihari-maredumilli/vanavihari-maredumilli.component.ts
+++ src/app/modules/resorts/vanavihari-maredumilli/vanavihari-maredumilli.component.ts
@@ -68,117 +68,117 @@ export class VanavihariMaredumilliComponent {
   // }));
 
   fetchRoomList() {
-    // let perm = '';
-    // if(this.selectedResort) perm += `&resort=${this.selectedResort}`;
-    // if(this.checkinDate) perm += `&checkin=${this.checkinDate}`;
-    // if(this.checkoutDate) perm += `&checkout=${this.checkoutDate}`;
-    // this.http
-    //   .get<any>(
-    //     'https://vanavihari-ng.netlify.app/zoho-connect?api_type=room_list'+perm
-    //   )
-    //   .subscribe({
-    //     next: (response) => {
-    //       if (response.code === 3000 && response.result.status === 'success') {
-    //         const json = response.result.data;
-    //         const jsonArray = Object.keys(json).map(key => {
-    //           return {
-    //             id: key,
-    //             ...json[key]
-    //           };
-    //         });
-    //         this.roomCards = this.mapRoomData(jsonArray, this.roomIds);
-    //       } else {
-    //         this.showErrorAlert(
-    //           'Failed to fetch room list. Please try again later.'
-    //         );
-    //       }
-    //       this.loadingRooms = false;
-    //     },
-    //     error: (err) => {
-    //       console.error('Error:', err);
-    //       this.showErrorAlert(
-    //         'An error occurred while fetching room list. Please try again later.'
-    //       );
-    //     },
-    // });
-    // setTimeout(() => {
-    //   this.loadingRooms = false;
-    // }, 2000);
-
-
-    
-    interface RoomDetails {
-      name: string;
-      week_end_guest_charge: number;
-      cottage_type: string;
-      week_day_rate: number;
-      max_adult: number;
-      max_child: number;
-      max_guest: number;
-      week_day_bed_charge: number,
-      id: number,
-      resort: string,
-      aminities: {
-        "4554333000000110021": string;
-        "4554333000000110025": string;
-        "4554333000000110017": string;
-      };
-      // Add other properties as needed
-    }
-    
-    // // Sample JSON object with the defined type
-    const json: { [key: string]: RoomDetails } = {
-      "4554333000000110059": {
-        name: "Room1",
-        week_end_guest_charge: 700,
-        cottage_type: "Hill Top Guest House",
-        week_day_rate: 2500,
-        max_adult: 2,
-        max_child: 1,
-        max_guest: 1,
-        week_day_bed_charge: 500,
-        id: 301,
-        resort: 'Vanavihari',
-        aminities: {
-          "4554333000000110021": "A/C",
-          "4554333000000110025": "Western",
-          "4554333000000110017": "Geyser"
+    let perm = '';
+    if(this.selectedResort) perm += `&resort=${this.selectedResort}`;
+    if(this.checkinDate) perm += `&checkin=${this.checkinDate}`;
+    if(this.checkoutDate) perm += `&checkout=${this.checkoutDate}`;
+    this.http
+      .get<any>(
+        'https://vanavihari-ng.netlify.app/zoho-connect?api_type=room_list'+perm
+      )
+      .subscribe({
+        next: (response) => {
+          if (response.code === 3000 && response.result.status === 'success') {
+            const json = response.result.data;
+            const jsonArray = Object.keys(json).map(key => {
+              return {
+                id: key,
+                ...json[key]
+              };
+            });
+            this.roomCards = this.mapRoomData(jsonArray, this.roomIds);
+          } else {
+            this.showErrorAlert(
+              'Failed to fetch room list. Please try again later.'
+            );
+          }
+          this.loadingRooms = false;
         },
-        // Other properties...
-      },
-      "4554333000000110065": {
-        name: "Room2",
-        week_end_guest_charge: 700,
-        cottage_type: "Pre-Fabricated Cottages",
-        week_day_rate: 2500,
-        max_adult: 2,
-        max_child: 1,
-        max_guest: 3,
-        week_day_bed_charge: 500,
-        id: 302,
-        resort: 'Vanavihari',
-        aminities: {
-          "4554333000000110021": "A/C",
-          "4554333000000110025": "Western",
-          "4554333000000110017": "Geyser"
+        error: (err) => {
+          console.error('Error:', err);
+          this.showErrorAlert(
+            'An error occurred while fetching room list. Please try again later.'
+          );
         },
-        // Other properties...
-      },
-      // Add more objects...
-      
-    };
-    const jsonArray = Object.keys(json).map(key => {
-        return json[key];
-        // return {
-        //   id: key,
-        //   ...json[key]
-        // };
     });
-    this.roomCards = this.mapRoomData(jsonArray, this.roomIds);
-
     setTimeout(() => {
-       this.loadingRooms = false;
+      this.loadingRooms = false;
     }, 2000);
+
+
+    
+    // interface RoomDetails {
+    //   name: string;
+    //   week_end_guest_charge: number;
+    //   cottage_type: string;
+    //   week_day_rate: number;
+    //   max_adult: number;
+    //   max_child: number;
+    //   max_guest: number;
+    //   week_day_bed_charge: number,
+    //   id: number,
+    //   resort: string,
+    //   aminities: {
+    //     "4554333000000110021": string;
+    //     "4554333000000110025": string;
+    //     "4554333000000110017": string;
+    //   };
+    //   // Add other properties as needed
+    // }
+    
+    // // // Sample JSON object with the defined type
+    // const json: { [key: string]: RoomDetails } = {
+    //   "4554333000000110059": {
+    //     name: "Room1",
+    //     week_end_guest_charge: 700,
+    //     cottage_type: "Hill Top Guest House",
+    //     week_day_rate: 2500,
+    //     max_adult: 2,
+    //     max_child: 1,
+    //     max_guest: 1,
+    //     week_day_bed_charge: 500,
+    //     id: 301,
+    //     resort: 'Vanavihari',
+    //     aminities: {
+    //       "4554333000000110021": "A/C",
+    //       "4554333000000110025": "Western",
+    //       "4554333000000110017": "Geyser"
+    //     },
+    //     // Other properties...
+    //   },
+    //   "4554333000000110065": {
+    //     name: "Room2",
+    //     week_end_guest_charge: 700,
+    //     cottage_type: "Pre-Fabricated Cottages",
+    //     week_day_rate: 2500,
+    //     max_adult: 2,
+    //     max_child: 1,
+    //     max_guest: 3,
+    //     week_day_bed_charge: 500,
+    //     id: 302,
+    //     resort: 'Vanavihari',
+    //     aminities: {
+    //       "4554333000000110021": "A/C",
+    //       "4554333000000110025": "Western",
+    //       "4554333000000110017": "Geyser"
+    //     },
+    //     // Other properties...
+    //   },
+    //   // Add more objects...
+      
+    // };
+    // const jsonArray = Object.keys(json).map(key => {
+    //     return json[key];
+    //     // return {
+    //     //   id: key,
+    //     //   ...json[key]
+    //     // };
+    // });
+    // this.roomCards = this.mapRoomData(jsonArray, this.roomIds);
+
+    // setTimeout(() => {
+    //    this.loadingRooms = false;
+    // }, 2000);
   }
  
   removeRoom(room: any, roomId: any) {    
