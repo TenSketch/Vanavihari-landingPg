91168559562a6a9d33579d13aa124e8908ca5e95
Venkat408 <venkat408prabhu@gmail.com>
2024-03-22 20:33:52 +0530

    myprofile/sign up fix - changes 2 - 22-03-2023

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -100,7 +100,7 @@ export class SignUpComponent implements OnInit {
       const params = new HttpParams()
         .set('fullname', this.form.value.full_name)
         .set('email', this.form.value.email_id)
-        .set('phone', this.form.value.mobile_number)
+        .set('mobile', this.form.value.mobile_number)
         .set('password', this.form.value.password);
       
       this.http
